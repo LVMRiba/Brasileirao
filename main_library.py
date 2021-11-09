ffb37c33ef71d84667aded353042331aa3c24264
Luiz Victor Moraes Riba <73717405+LVMRiba@users.noreply.github.com>
2021-11-09 16:56:06 -0300

    0.32

---
+++ main_library.py
@@ -0,0 +1,11 @@
+# LIBRARY
+import pandas as pd         # BIBLIOTECA DE ANÁLISE DE DADOS
+from datetime import date   # BIBLIOTECA DE TEMPO
+import codecs               # HABILITAR UTF-8
+from tkinter import *       # BIBLIOTECA DE INTERFACE GRÁFICA
+from tkinter import ttk     # BIBLIOTECA PARA COMBOBOX
+
+# MODULES
+from files import Files
+from generators import Gens
+from manager import ETL
